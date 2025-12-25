3428837e58f6a968a58c847a5e6a8967db2dab5c
Jakub Tehlar <tehlarjakub@gmail.com>
2025-12-25 22:15:43 +0100

    add command types

--- bot/globvars.py
+++ bot/globvars.py
@@ -11,6 +11,16 @@ ENCODING_VARIANT_ID = 1  # Identifier for the non-standard base64 alphabet
 ROOT_SECRET = "supersecretkey"  # Shared secret key for authentication
 MAGIC_BYTES = b'\xAB\xCD'  # Example magic bytes for frame start
 
+# COMMAND TYPES
+CMD_TYPES = {
+    "announce": 0x01,
+    "list_users": 0x02,
+    "list_dir": 0x03,
+    "user_id": 0x04,
+    "copy_file": 0x05,
+    "exec_binary": 0x06
+}
+
 # Encoding 
 STANDARD_ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
 CUSTOM_ALPHABET   = "ZYXWVUTSRQPONMLKJIHGFEDCBAzyxwvutsrqponmlkjihgfedcba9876543210+/"
